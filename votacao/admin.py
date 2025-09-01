644ca068a834c713909b794ed05c2476bdac8afd
Amanda <amandalais06@gmail.com>
2025-09-01 20:27:49 -0300

    primeira página, modelos, admin, views e urls

--- votacao/admin.py
+++ votacao/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Eleicao, Eleitor, Chapa
 
 # Register your models here.
+@admin.register(Eleicao)
+class EleicaoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'descricao', 'data_termino')
+
+@admin.register(Chapa)
+class ChapaAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'coordenador', 'qtd_votos', 'eleita')
--- votacao/models.py
+++ votacao/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
 # Create your models here.
+class Administrador(models.Model):
+    matricula = models.CharField(max_length=50)
+    senha = models.CharField(max_length=50)
+    adm = True
+
+class Eleicao(models.Model):
+    titulo = models.CharField(max_length=50)
+    descricao = models.CharField(max_length=200)
+    data_termino = models.DateTimeField()
+
+    def __str__(self):
+        return self.titulo
+
+class Chapa(models.Model):
+    nome = models.CharField(max_length=50)
+    coordenador = models.CharField(max_length=50)
+    descricao = models.CharField(max_length=200)
+    qtd_votos = models.IntegerField()
+    eleita = models.BooleanField()
+    eleicao = models.ForeignKey(Eleicao, on_delete=models.CASCADE, related_name='chapas')
+
+    def __str__(self):
+        return self.nome
+
+class Eleitor(models.Model):
+    matricula = models.CharField(max_length=50)
+    senha = models.CharField(max_length=50)
+    adm = False
+
+    def __str__(self):
+        return self.matricula
+
+class Voto(models.Model):
+    eleitor = models.ForeignKey(Eleitor, on_delete=models.CASCADE, related_name="votos")
+    eleicao = models.ForeignKey(Eleicao, on_delete=models.CASCADE, related_name="votos")
+    chapa = models.ForeignKey(Chapa, on_delete=models.CASCADE, related_name="votos")
+    data_hora = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ("eleitor", "eleicao")
+
+    def __str__(self):
+        return f"{self.eleitor.nome} votou na {self.chapa.nome} ({self.eleicao.titulo})"
